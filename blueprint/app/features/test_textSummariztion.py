7e10f67b259b05e13f4aa6d701083a5c5830e66c
miguel angel <miguelsalvador355@gmail.com>
2023-03-11 19:25:44 -0700

    refactoring multiple python scripts into a single module for needed text processing

--- blueprint/app/features/test_textSummariztion.py
+++ blueprint/app/features/test_textSummariztion.py
@@ -6,10 +6,23 @@ from textSummarization import SummarizeText
 path = '/home/migui/school/spring23/seniorXp/project/Project-Generator/data/sampleTexts/MSalvador_FinalPromptAssesment.txt'
 s1 = SummarizeText(path)
 #s1.summarizeText()
-result = s1.summarizeText()
-print("\n", str(result))
+sumText = s1.summarizeText()
+print(sumText)
 
-#print out results to a txt file
-with open("/home/migui/school/spring23/seniorXp/project/Project-Generator/data/sampleTexts/oneThreeResult.txt",'w') as f:
-    f.write(str(result))
-    f.close
+##print out results to a txt file
+#with open("/home/migui/school/spring23/seniorXp/project/Project-Generator/data/sampleTexts/oneThreeResult.txt",'w') as f:
+    #f.write(str(result))
+    #f.close
+
+
+
+
+#summ = pipeline("summarization", model='lidiya/bart-large-xsum-samsum')
+#path = '/home/migui/school/spring23/seniorXp/project/Project-Generator/data/sampleTexts/MSalvador_FinalPromptAssesment.txt'
+#with open(path) as f:
+    #text = f.read()
+    #f.close()
+
+#results = summ(text)
+#for results in results:
+    #print(results['summary_text'])
--- blueprint/app/features/textSummarization.py
+++ blueprint/app/features/textSummarization.py
@@ -14,16 +14,17 @@ class SummarizeText:
         with open(self.genText) as f:
             sampleText = f.read()
             f.close
-        return(summarizer(sampleText))
+        results = summarizer(sampleText)
+        for results in results:
+            return(results['summary_text'])
 
-
-    
-##minor testing
+# minor testing
 if __name__ == "__main__":
     #initialize the model
     #summarizer = pipeline("summarization", model="lidiya/bart-large-xsum-samsum")
 
     #ask user for input
-    path = input('File path: ')
+    #path = input('File path: ')
+    path = '/home/migui/school/spring23/seniorXp/project/Project-Generator/data/sampleTexts/MSalvador_FinalPromptAssesment.txt'
     result = SummarizeText(path)
     print(result.summarizeText())
