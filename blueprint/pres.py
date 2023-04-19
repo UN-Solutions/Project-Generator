0c2d7a1b89128ab2f9f122b4a43af959c5d1ee33
Rigoberto <hinojosrigo@gmail.com>
2023-04-19 10:37:49 -0600

    Fixed the presentation python module and got it working

--- blueprint/pres.py
+++ blueprint/pres.py
@@ -2,70 +2,49 @@ import sys
 import openai
 from fpdf import FPDF
 import re
-
+import unicodedata
 openai.api_key = ''
-input_string = ''
-for i in range(1, len(sys.argv)):
-    arg = sys.argv[i]
-    input_string += arg
-
-# Split the string into a list of key-value pairs
+input_string = 'title:1800 History in Russia, subject: war, time period, deaths, subtopics: how it shaped where we are now'
 kv_pairs = input_string.split(',')
 
-class TriFoldPDF(FPDF):
-    pass
+class PowerPointPDF(FPDF):
 
     def __init__(self):
-        super().__init__(orientation = 'L') # Set the orientation
-        self.page_size = (279.4, 215.9) # A4 paper size in mm 1056x816 pixels
-        self.left_margin = 1 # mm
-        self.right_margin = -16 # mm
-        self.top_margin = 1 # mm
-        self.bottom_margin = 10 # mm
-        self.column_gap = 0 # mm
-        self.column_width = (self.page_size[0] - self.left_margin - self.right_margin - self.column_gap*2) / 3
+        super().__init__('L', 'in', (7.5, 10))  # Landscape orientation, inches as units, and custom page size
+        self.set_margins(0.5, 0.5, 0.5)  # Set 0.5-inch margins
 
-    def draw_columns(self):
-        x = self.left_margin
-        y = self.top_margin
-        for i in range(3):
-            self.rect(x, y, self.column_width, self.page_size[1] - self.top_margin - self.bottom_margin, 'D')
-            x += self.column_width + self.column_gap
+    def draw_border(self):
+        self.set_line_width(0.05)  # Set the line width for the border
+        self.rect(0.5, 0.5, 9, 6.5)  # Draw a rectangle as the border
 
-    def get_multiline_cell_height(self, width, text):
+    def sanitize_text(self, text):
+        return ''.join(c for c in unicodedata.normalize('NFKD', text) if ord(c) < 256)
+    def add_slide(self, title, content):
+        self.add_page()
+        self.draw_border()
+        self.set_font("Arial", 'B', 24)  # Set the font for the title
+        self.set_xy(1, 1)  # Set the position for the title
+        self.cell(8, 1, title, 0, 1, 'C')  # Add the title
+
+        self.set_font("Arial", '', 12)  # Set the font for the content
+        self.set_xy(1, 2)  # Set the position for the content
+        content = self.fit_text_to_slide(content)
+        self.multi_cell(8, 0.5, content, 0, 'C')  # Add the content
+
+    def fit_text_to_slide(self, text):
+        max_lines = int((6.5 - 2) / 0.5)  # Calculate the maximum number of lines that can fit in the slide
         lines = text.split('\n')
-        total_height = 0
-        for line in lines:
-            line_width = self.get_string_width(line)
-            if line_width <= width:
-                total_height += self.font_size
-            else:
-                # calculate height of line with word wrap
-                words = line.split()
-                line = ''
-                for word in words:
-                    if self.get_string_width(line + ' ' + word) <= width:
-                        line += ' ' + word
-                    else:
-                        total_height += self.font_size
-                        line = word
-                total_height += self.font_size
-        return total_height
-
-
-    def add_content(self, title, body, x, y):
-        self.set_xy(x, y)
-        self.set_font('Arial', 'B', 14)
-        self.cell(self.column_width - 10, 10, title, 0, 1)
-        self.set_xy(x, y + 12)
-        self.set_font('Arial', '', 8)
-        cell_width = self.column_width - 9
-        cell_height = self.get_multiline_cell_height(cell_width, body)
-        self.multi_cell(self.column_width - 9, 8, body)
-
+        if len(lines) > max_lines:
+            lines = lines[:max_lines]
+            lines[-1] = lines[-1].rstrip() + '...'  # Add an ellipsis to the last line
+            text = '\n'.join(lines)
+        return text
 
 def chatgpt(kv_pairs):
     # Loop through each key-value pair and extract the value for the desired keys
+    title = ''
+    subject = ''
+    subtopics = ''
 
     for kv in kv_pairs:
         if 'title:' in kv:
@@ -80,12 +59,12 @@ def chatgpt(kv_pairs):
     print('Subject:', subject)
     print('Subtopics:', subtopics)
 
-    ask = ('Can you write a report with the main title going to be '+ title + ', and the main subject matter is about the ' + subject + ' with subtopics of ' + subtopics + '. could you give me an Problems & Affects, Background Research, Prevention Steps, and a Conclusion section. If you could give both the  Problema & Affects and prevention steps as a list with each point starting with "-". add a double enter at the end of each section. use a ":" behind the title of sections. I do not need a main title given. Start with Problems & Affects, have the background research be one paragarph around 100 words, and the Conclusion be under 60 words.')
+    ask = ('Can you write a report with the main title going to be '+ title + ', and the main subject matter is about the ' + subject + ' with subtopics of ' + subtopics + '. could you give me an abstract, background research, results, a conclusion, and a future directions section. add a double enter at the end of each section. use a ":" behind the title of sections. I do not need a main title given.')
     response = openai.Completion.create(
         model="text-davinci-003",
         prompt=ask,
         temperature=0.9,
-        max_tokens=700,
+        max_tokens=1000,
         top_p=1,
         frequency_penalty=0,
         presence_penalty=0.6,
@@ -97,33 +76,36 @@ def chatgpt(kv_pairs):
 
     return text, title, subject, subtopics
 
+
 text, title, subject, subtopics = chatgpt(kv_pairs)
 
-sections = text.split('\n\n')
-problems_and_affects = sections[1]
-problems_and_affects = re.sub(r"Problems & Affects: \n", "", problems_and_affects)
-background_research = sections[2]
-background_research = re.sub(r"Background Research: \n", "", background_research)
-prevention_steps = sections[3]
-prevention_steps = re.sub(r"Preventions Steps: \n", "", prevention_steps)
-conclusion = sections[4]
-conclusion = re.sub(r"Conclusion: \n", "", conclusion)
+# Define regular expression patterns to match the section titles
+abstract_pattern = re.compile(r"Abstract:\s*(.*)\n\n")
+background_pattern = re.compile(r"Background Research:\s*(.*)\n\n")
+results_pattern = re.compile(r"Results:\s*(.*)\n\n")
+conclusion_pattern = re.compile(r"Conclusion:\s*(.*)\n\n")
+future_pattern = re.compile(r"Future Directions:\s*(.*)")
 
+# Extract the text for each section
+abstract = abstract_pattern.search(text).group(1)
+background = background_pattern.search(text).group(1)
+results = results_pattern.search(text).group(1)
+conclusion = conclusion_pattern.search(text).group(1)
+future = future_pattern.search(text).group(1)
 
+# Print the results
+print("Abstract:", abstract)
+print("Background Research:", background)
+print("Results:", results)
+print("Conclusion:", conclusion)
+print("Future Directions:", future)
+pdf = PowerPointPDF()
 
-print(problems_and_affects)
-print(background_research)
-print(prevention_steps)
-print(conclusion)
+# Add slides with titles and content
+pdf.add_slide(title, subject)
+pdf.add_slide("", background)
+pdf.add_slide("", results)
+pdf.add_slide("", conclusion)
 
+pdf.output("PowerPointStyle.pdf", "F")
 
-pdf = TriFoldPDF()
-pdf.add_page()
-# Add the title to the top of the PDF
-pdf.draw_columns()
-pdf.add_content('Problems & Affects', problems_and_affects,5,30)
-pdf.add_content('Background Research:', background_research,100, 40)
-pdf.add_content(title, '',100,15)
-pdf.add_content('Prevention Steps', prevention_steps,200,15)
-pdf.add_content('Conclusion', conclusion,200,125)
-pdf.output('awareness.pdf', 'F')
